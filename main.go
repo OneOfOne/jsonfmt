a52ca3680617d24ffc9038b18e8b2b1943744c42
Ahmed (OneOfOne) W <oneofone@gmail.com>
2015-01-10 00:07:36 +0200

    inplace formatting and multiple files

--- main.go
+++ main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 )
 
+var (
+	inPlace = flag.Bool("i", false, "format in place")
+)
+
 func main() {
-	f, err := os.Open(os.Args[1])
-	dieIf(err)
-	var v interface{}
-	dieIf(json.NewDecoder(f).Decode(&v))
-	f.Close()
-	j, _ := json.MarshalIndent(v, "", "\t")
-	os.Stdout.Write(j)
+	flag.Parse()
+	for _, fn := range flag.Args() {
+		fmt.Fprintf(os.Stderr, "formatting %s\n", fn)
+		f, err := os.Open(fn)
+		dieIf(err)
+		var v interface{}
+		json.NewDecoder(f).Decode(&v)
+		f.Close()
+		j, _ := json.MarshalIndent(v, "", "\t")
+		if *inPlace {
+			f, err = os.Create(fn)
+			dieIf(err)
+			fmt.Fprintf(f, "%s\n", j)
+			f.Close()
+		} else {
+			fmt.Printf("%s\n", j)
+
+		}
+	}
 }
 
 func dieIf(err interface{}) {
